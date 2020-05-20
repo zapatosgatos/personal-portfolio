6a48aee122e334b5a4d80c18207dd45bc2f3818b
zapatosgatos <bostwicm@gmail.com>
2020-05-20 14:43:14 -0600

    Still testing

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ def hello_world():
     return 'New updates'
 
 @app.route('/update_server', methods=['POST'])
-    def webhook():
-        if request.method == 'POST':
-            repo = git.Repo('path/to/git_repo')
-            origin = repo.remotes.origin
-origin.pull()
-return 'Updated PythonAnywhere successfully', 200
-        else:
-            return 'Wrong event type', 400
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('path/to/git_repo')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400

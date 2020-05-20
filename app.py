548ac3d91a11c28a454ea9e6fd1767169851bc62
zapatosgatos <bostwicm@gmail.com>
2020-05-20 14:27:14 -0600

    Adding git webhooks

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, request
+import git
 
 app = Flask(__name__)
 
@@ -9,3 +10,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello from Flask!'
 
+@app.route('/update_server', methods=['POST'])
+    def webhook():
+        if request.method == 'POST':
+            repo = git.Repo('path/to/git_repo')
+            origin = repo.remotes.origin
+origin.pull()
+return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Wrong event type', 400

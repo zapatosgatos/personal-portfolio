05e75539cd0e639a0b58ac2cc21ad320dbe661cc
zapatosgatos <bostwicm@gmail.com>
2020-05-20 19:30:58 -0600

    Updating webhook security

--- app.py
+++ app.py
@@ -3,8 +3,22 @@
 
 from flask import Flask, request
 import git
+import hmac
+import hashlib
+import os
+
+github_signature = os.getenv("GIT_SECRET_KEY")
 
 app = Flask(__name__)
+
+def is_valid_signature(x_hub_signature, data, private_key):
+    # x_hub_signature and data are from the webhook payload
+    # private key is your webhook secret
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 @app.route('/')
 def hello_world():
@@ -13,9 +27,13 @@ def hello_world():
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('/home/zapatosgatos/personal_portfolio')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Updated PythonAnywhere successfully', 200
+        x_hub_signature = request.headers.get(‘X-Hub-Signature’)
+        if is_valid_signature(x_hub_signature, request.data, github_signature):
+            repo = git.Repo('/home/zapatosgatos/personal_portfolio')
+            origin = repo.remotes.origin
+            origin.pull()
+            return 'Updated PythonAnywhere successfully', 200
+        else:
+            return 'Not valid signature', 400
     else:
         return 'Wrong event type', 400

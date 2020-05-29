3d2e8ddcecaa01309540405168f3cdec655ce5a8
zapatosgatos <bostwicm@gmail.com>
2020-05-28 21:36:53 -0600

    Updating sunburst code

--- personal-portfolio/static/js/spotify.js
+++ personal-portfolio/static/js/spotify.js
@@ -23,6 +23,102 @@ $(function(){
         console.log(response);
         var width = document.getElementById('searchResults').clientWidth;
         var height = width;
+        //var svg = d3.select('#searchResults').append('svg');
+        const root = partition(data);
+
+        root.each(d => d.current = d);
+
+        const svg = d3.select('#searchResults').append('svg');
+            .attr("viewBox", [0, 0, width, width])
+            .style("font", "10px sans-serif");
+
+        const g = svg.append("g")
+            .attr("transform", `translate(${width / 2},${width / 2})`);
+
+        const path = g.append("g")
+          .selectAll("path")
+          .data(root.descendants().slice(1))
+          .join("path")
+            .attr("fill", d => { while (d.depth > 1) d = d.parent; return color(d.data.name); })
+            .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
+            .attr("d", d => arc(d.current));
+
+        path.filter(d => d.children)
+            .style("cursor", "pointer")
+            .on("click", clicked);
+
+        path.append("title")
+            .text(d => `${d.ancestors().map(d => d.data.name).reverse().join("/")}\n${format(d.value)}`);
+
+        const label = g.append("g")
+            .attr("pointer-events", "none")
+            .attr("text-anchor", "middle")
+            .style("user-select", "none")
+          .selectAll("text")
+          .data(root.descendants().slice(1))
+          .join("text")
+            .attr("dy", "0.35em")
+            .attr("fill-opacity", d => +labelVisible(d.current))
+            .attr("transform", d => labelTransform(d.current))
+            .text(d => d.data.name);
+
+        const parent = g.append("circle")
+            .datum(root)
+            .attr("r", radius)
+            .attr("fill", "none")
+            .attr("pointer-events", "all")
+            .on("click", clicked);
+
+        function clicked(p) {
+          parent.datum(p.parent || root);
+
+          root.each(d => d.target = {
+            x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+            x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+            y0: Math.max(0, d.y0 - p.depth),
+            y1: Math.max(0, d.y1 - p.depth)
+          });
+
+          const t = g.transition().duration(750);
+
+          // Transition the data on all arcs, even the ones that aren’t visible,
+          // so that if this transition is interrupted, entering arcs will start
+          // the next transition from the desired position.
+          path.transition(t)
+              .tween("data", d => {
+                const i = d3.interpolate(d.current, d.target);
+                return t => d.current = i(t);
+              })
+            .filter(function(d) {
+              return +this.getAttribute("fill-opacity") || arcVisible(d.target);
+            })
+              .attr("fill-opacity", d => arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+              .attrTween("d", d => () => arc(d.current));
+
+          label.filter(function(d) {
+              return +this.getAttribute("fill-opacity") || labelVisible(d.target);
+            }).transition(t)
+              .attr("fill-opacity", d => +labelVisible(d.target))
+              .attrTween("transform", d => () => labelTransform(d.current));
+        }
+
+        function arcVisible(d) {
+          return d.y1 <= 3 && d.y0 >= 1 && d.x1 > d.x0;
+        }
+
+        function labelVisible(d) {
+          return d.y1 <= 3 && d.y0 >= 1 && (d.y1 - d.y0) * (d.x1 - d.x0) > 0.03;
+        }
+
+        function labelTransform(d) {
+          const x = (d.x0 + d.x1) / 2 * 180 / Math.PI;
+          const y = (d.y0 + d.y1) / 2 * radius;
+          return `rotate(${x - 90}) translate(${y},0) rotate(${x < 180 ? 0 : 180})`;
+        }
+
+        return svg.node();
+        /*var width = document.getElementById('searchResults').clientWidth;
+        var height = width;
         var radius = Math.min(width, height) / 2;
         var color = d3.scaleOrdinal(d3.schemeCategory20b);
         var svg = d3.select('#searchResults').append('svg');
@@ -30,25 +126,9 @@ $(function(){
         var g = d3.select('#searchResults svg')
           .attr('width', width)
           .attr('height', height)
+          .style("font", "10px sans-serif");
           .append('g')
           .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-
-        /*function resize() {
-          width = document.getElementById('searchResults').clientWidth;
-          height = width;
-          radius = Math.min(width, height) / 2;
-          //var color = d3.scaleOrdinal(d3.schemeCategory20b);
-
-          g = d3.select('#searchResults svg')
-            .attr('width', width)
-            .attr('height', height)
-            .append('g')
-            .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-        }*/
-
-        //window.onresize = resize;
-        //var radius = Math.min(width, height) / 2;
-
 
         // Data strucure
         var partition = d3.partition()
@@ -75,6 +155,11 @@ $(function(){
             .attr("d", arc)
             .style('stroke', '#fff')
             .style("fill", function (d) { return color((d.children ? d : d.parent).data.name); });
+            .attr("fill-opacity", d => arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
+
+        path.filter(d => d.children)
+          .style("cursor", "pointer")
+          .on("click", clicked);
 
         g.selectAll(".node")
             .append("text")
@@ -82,15 +167,17 @@ $(function(){
                 return "translate(" + arc.centroid(d) + ")rotate(" + computeTextRotation(d) + ")"; })
             .attr("dx", "-20") // radius margin
             .attr("dy", ".5em") // rotation align
-            .text(function(d) { return d.parent ? d.data.name : artist });
+            .text(function(d) { return d.parent ? d.data.name : "" });
 
         function computeTextRotation(d) {
           var angle = (d.x0 + d.x1) / Math.PI * 90;
 
           // Avoid upside-down labels
-          //return (angle < 120 || angle > 270) ? angle : angle + 180;  // labels as rims
-          return (angle < 180) ? angle - 90 : angle + 90;  // labels as spokes
-        }
+          return (angle < 120 || angle > 270) ? angle : angle + 180;  // labels as rims
+          //return (angle < 180) ? angle - 90 : angle + 90;  // labels as spokes
+        }*/
+
+
 			},
 			error: function(error){
 				console.log(error);
